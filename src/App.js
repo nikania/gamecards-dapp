f553d20a6bb5b1d14cc3cbc95b79796b2836f00f
nikania <nianemis@gmail.com>
2022-03-19 16:12:23 +0100

    simple route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Login from './pages/Login';
+import Cards from './pages/Cards';
+import Profile from './pages/Profile';
+import { Route, Router } from 'wouter';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-       Gamecards dapp
-      </header>
-    </div>
+    <Router>
+      <Route path='/' component={Cards}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/profile' component={Profile}/>
+    </Router>   
   );
 }
 
